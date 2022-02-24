c6840bba9b24d9c9d9bf8f1f24a7f207b945c5bb
EC2 Default User <ec2-user@ip-172-31-4-109.ec2.internal>
2022-02-24 09:50:16 +0000

    [Finish]ex-3

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,10 @@
 class Book < ApplicationRecord
   has_one_attached :profile_image
   belongs_to :user, optional: true
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books, dependent: :destroy
+  has_many :books
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -13,5 +15,9 @@ class User < ApplicationRecord
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
 end
