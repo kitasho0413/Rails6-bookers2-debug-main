1c9effa27067d291735c53d15a46120192f31f1f
EC2 Default User <ec2-user@ip-172-31-4-109.ec2.internal>
2022-03-07 06:27:36 +0000

    [Finish]ex-6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,31 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery"
-import "popper.js"
-import "bootstrap"
-import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"
+import "@fortawesome/fontawesome-free/js/all"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-//= require jquery
-//= require rails-ujs
-require("@rails/ujs").start()
-// require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
