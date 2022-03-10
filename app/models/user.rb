ec8ac4988955ad03d3dc54c88246736183cfed47
EC2 Default User <ec2-user@ip-172-31-4-109.ec2.internal>
2022-03-10 05:59:31 +0000

    [Finish]ex-7c

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,9 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
   
+  has_many :group_users
+  has_many :groups, through: :group_users
+  
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
   
   resources :messages, only: [:create]
   resources :rooms, only: [:create, :show]
+  
+  resources :groups, exept: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
